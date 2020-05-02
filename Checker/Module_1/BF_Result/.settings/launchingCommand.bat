c78ecced35c16fd031fe68de60d7ff0f984780c6
xueshawu <786494921@qq.com>
2020-05-02 09:34:00 +0800

    1.增加PolySpace代码检查工程 2.增加Systick相关文件

---
+++ Checker/Module_1/BF_Result/.settings/launchingCommand.bat
@@ -0,0 +1,2 @@
+@echo off
+"D:\MATLAB\polyspace\bin\polyspace-bug-finder-nodesktop.exe" -options-file "E:\mywork\myproject\My_Github\S32K144\Checker\Module_1\BF_Result\.settings\options_command.txt" -results-dir "E:\mywork\myproject\My_Github\S32K144\Checker\Module_1\BF_Result" %*
